2da95f63f67d963255ce2de99352e279d100cef9
SavelyPos <gerchili@yandex.ru>
2020-09-11 22:49:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Author author = new Author("Sava", "Sava@mail.ru", 'M');
+        author.setEmail("gerchili@yandex.ru");
+        System.out.println(author);
+
+    }
+
+}
